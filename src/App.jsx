354472ab6a8e29c0a04cc7303aafa627783b1dd7
MaximoCarreras <m.ezequielcarreras@gmail.com>
2026-06-11 21:52:08 -0300

    ca

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home'; 
-import Bodegas from './pages/Bodegas/Bodegas'; /* <-- NUEVO: Importamos la página */
+import Bodegas from './pages/Bodegas/Bodegas';
 import './App.css';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/bodegas" element={<Bodegas />} /> /* <-- NUEVO: Creamos la ruta */
+      <Route path="/bodegas" element={<Bodegas />} />
     </Routes>
   );
 }
