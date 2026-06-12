9235190516f12848b29106fbb85e2a9c63c176a8
MaximoCarreras <m.ezequielcarreras@gmail.com>
2026-06-11 21:32:44 -0300

    ca

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home'; /* Asegurate de que esta ruta coincida con donde guardaste el Home.jsx */
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home'; 
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
