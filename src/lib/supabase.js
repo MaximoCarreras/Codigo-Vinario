fb8d4a249e20f0185ddd6e786ebed52e1fbb977f
MaximoCarreras <m.ezequielcarreras@gmail.com>
2026-06-12 03:53:13 -0300

    supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://bbqnusdjanbusrrtsotg.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJicW51c2RqYW5idXNycnRzb3RnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5NTE1MjYsImV4cCI6MjA5MzUyNzUyNn0.9jP_xlQIrHHD8iRyyMKYUdVF0Q8PE-349AtFGAdbIcw'
+const supabaseUrl = 'https://kakwiqtnpudmztfybpav.supabase.co/rest/v1/'
+const supabaseAnonKey = 'sb_publishable_RwYNY5aOh4nJdpYXFVoA6A_xGXLFtPY'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
