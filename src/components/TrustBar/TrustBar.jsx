cc95d18f3df3c30bf6b27e74fcfa91a077fe7a9c
MaximoCarreras <m.ezequielcarreras@gmail.com>
2026-06-12 03:31:48 -0300

    TrustBar implementada con estilo corporativo Código Vinario

--- src/components/TrustBar/TrustBar.jsx
+++ src/components/TrustBar/TrustBar.jsx
@@ -1,27 +1,45 @@
 import React from 'react';
 import './TrustBar.css';
 
-const TrustBar = () => {
-  return (
-    <div className="cv-trustbar">
-      <div className="trust-item">
-        <span className="trust-icon">👍</span>
-        <p>98% vuelve por más</p>
-      </div>
-      <div className="trust-item">
-        <span className="trust-icon">⭐</span>
-        <p>Calificación 5 estrellas</p>
-      </div>
-      <div className="trust-item">
-        <span className="trust-icon">📦</span>
-        <p>Envíos a todo el país</p>
-      </div>
-      <div className="trust-item">
-        <span className="trust-icon">🍷</span>
-        <p>Selección exclusiva</p>
-      </div>
-    </div>
-  );
-};
+export default function TrustBar() {
+  const trustFeatures = [
+    {
+      icon: 'local_shipping',
+      title: 'Envíos a todo el país',
+      desc: 'Logística segura y garantizada.'
+    },
+    {
+      icon: 'credit_card',
+      title: 'Pagos Seguros',
+      desc: '3 cuotas sin interés.'
+    },
+    {
+      icon: 'wine_bar',
+      title: 'Selección Experta',
+      desc: 'Curaduría premium de bodegas.'
+    },
+    {
+      icon: 'support_agent',
+      title: 'Asesoramiento',
+      desc: 'Trato directo y personal.'
+    }
+  ];
 
-export default TrustBar;
+  return (
+    <section className="cv-trustbar">
+      <div className="cv-trustbar-container">
+        {trustFeatures.map((feature, index) => (
+          <div className="cv-trustbar-item" key={index}>
+            <span className="material-symbols-outlined cv-trustbar-icon">
+              {feature.icon}
+            </span>
+            <div className="cv-trustbar-text">
+              <h4 className="cv-trustbar-title">{feature.title}</h4>
+              <p className="cv-trustbar-desc">{feature.desc}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
