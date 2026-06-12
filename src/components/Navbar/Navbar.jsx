1f066771554762fc07a537ec67138c69204dfbdf
MaximoCarreras <m.ezequielcarreras@gmail.com>
2026-06-11 23:56:18 -0300

    Navbar completa y profesional

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,7 +13,6 @@ const Navbar = () => {
       {/* Main Navbar */}
       <div className="cv-navbar-main">
         <div className="cv-logo">
-          {/* Si querés que el logo te lleve al inicio al hacerle clic, también usamos Link acá */}
           <Link to="/" className="logo-text" style={{ textDecoration: 'none' }}>
             <span className="binary-text">01010110</span>
             <h1 style={{ color: 'var(--color-negro)' }}>CÓDIGO VINARIO</h1>
@@ -36,11 +35,9 @@ const Navbar = () => {
         <ul>
           <li><Link to="/bodegas">BODEGAS</Link></li>
           
-          {/* Este es el ítem que despliega el Mega Menú */}
           <li className="has-mega-menu">
             <Link to="/categoria/vinos">VINOS <span>▾</span></Link>
             
-            {/* Contenedor del Mega Menú */}
             <div className="mega-menu">
               <div className="mega-menu-content">
                 <div className="mega-column">
@@ -49,26 +46,23 @@ const Navbar = () => {
                   <Link to="/categoria/cabernet">Cabernet Sauvignon</Link>
                   <Link to="/categoria/cabernet-franc">Cabernet Franc</Link>
                   <Link to="/categoria/blend">Blend</Link>
-                  <Link to="/categoria/pinot-noir">Pinot Noir</Link>
                 </div>
                 <div className="mega-column">
                   <h4>// BLANCOS</h4>
                   <Link to="/categoria/chardonnay">Chardonnay</Link>
                   <Link to="/categoria/sauvignon">Sauvignon Blanc</Link>
                   <Link to="/categoria/torrontes">Torrontés</Link>
-                  <Link to="/categoria/semillon">Semillón</Link>
                 </div>
                 <div className="mega-column">
                   <h4>// ESPUMANTES</h4>
                   <Link to="/categoria/brut-nature">Brut Nature</Link>
                   <Link to="/categoria/extra-brut">Extra Brut</Link>
-                  <Link to="/categoria/dulce">Espumante Dulce</Link>
+                  <Link to="/categoria/dulce">Dulce</Link>
                 </div>
                 <div className="mega-column">
                   <h4>// OTROS</h4>
                   <Link to="/categoria/rosados">Rosados</Link>
                   <Link to="/categoria/naranjos">Naranjos</Link>
-                  <Link to="/categoria/organicos">Orgánicos</Link>
                 </div>
               </div>
               
@@ -76,12 +70,10 @@ const Navbar = () => {
                 <Link to="/categoria/destacados">
                   <button className="cv-btn-white">VER SELECCIÓN</button>
                 </Link>
-                <img src="https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80" alt="Selección de Vinos" />
               </div>
             </div>
           </li>
 
-          {/* Enlaces directos en la barra principal */}
           <li><Link to="/categoria/destilados">DESTILADOS</Link></li>
           <li><Link to="/categoria/cervezas">CERVEZAS</Link></li>
           <li><Link to="/categoria/combos">COMBOS</Link></li>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,27 +1,38 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import Hero from '../../components/Hero/Hero';
-import TrustBar from '../../components/TrustBar/TrustBar';
-import CategoryBanners from '../../components/CategoryBanners/CategoryBanners';
-import ProductCarousel from '../../components/ProductCarousel/ProductCarousel';
-import LocalServices from '../../components/LocalServices/LocalServices';
-import Community from '../../components/Community/Community';
+import LogoLoop from '../../components/ReactBits/LogoLoop';
+import TiltedCard from '../../components/ReactBits/TiltedCard';
 import Footer from '../../components/Footer/Footer';
 
 const Home = () => {
+  const bodegas = [
+    { src: 'https://via.placeholder.com/150x50?text=PIXEL', alt: 'Pixel' },
+    { src: 'https://via.placeholder.com/150x50?text=CHACHINGO', alt: 'Chachingo' },
+    { src: 'https://via.placeholder.com/150x50?text=LAMADRID', alt: 'Lamadrid' },
+  ];
+
   return (
     <div>
       <Navbar />
       <Hero />
-      <TrustBar />
-      <ProductCarousel titulo="Combos X6 y X12" etiquetaCodigo="{ cajas_cerradas }" />
-      <CategoryBanners />
-      <LocalServices />
-      <ProductCarousel titulo="Botellones y Magnum" etiquetaCodigo="< grandes_formatos >" />
-      <Community />
+      
+      {/* Sección de Bodegas (LogoLoop) */}
+      <section style={{ padding: '40px 0', backgroundColor: '#f9f9f9' }}>
+         <LogoLoop logos={bodegas} speed={60} />
+      </section>
+
+      {/* Sección Productos 3D */}
+      <section style={{ display: 'flex', justifyContent: 'center', padding: '60px' }}>
+         <TiltedCard 
+           imageSrc="https://images.unsplash.com/photo-1584916201218-f4242ceb4809"
+           captionText="Vino Premium Pixel"
+         />
+      </section>
+
       <Footer />
     </div>
   );
 };
 
-export default Home; // <--- ESTA LÍNEA ES OBLIGATORIA AL FINAL
+export default Home;
