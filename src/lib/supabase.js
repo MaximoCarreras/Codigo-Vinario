6c0112c8ba967982f2bf9d1704b1bc9a61a6b9be
MaximoCarreras <m.ezequielcarreras@gmail.com>
2026-06-12 04:01:11 -0300

    a

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://kakwiqtnpudmztfybpav.supabase.co/rest/v1/'
-const supabaseAnonKey = 'sb_publishable_RwYNY5aOh4nJdpYXFVoA6A_xGXLFtPY'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
